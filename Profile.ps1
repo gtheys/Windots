4ac60c69c9c5abd6ee35d4f5ca90eeec93cb6d2c
Geert Theys <geert.theys@gmail.com>
2024-07-10 13:29:14 +0700

    Adding PSReadline
    Stop gap until I setup FZF properly in poweshell

--- Profile.ps1
+++ Profile.ps1
@@ -377,6 +377,21 @@ $enableLog ? $stopwatch.Stop() : $null
 
 Add-ProfileLogEntry -Message "Profile load complete"
 
+
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
+
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+
+
 # Skip fastfetch for non-interactive shells and vim terminals
 if ([Environment]::GetCommandLineArgs().Contains("-NonInteractive") -or [Environment]::GetCommandLineArgs().Contains("-CustomPipeName")) {
     return
